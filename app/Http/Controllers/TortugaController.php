5e3e740af6da41132249fe04c961109f0127f937
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-24 09:36:10 +0200

    Landing para redirigir tras el login

--- app/Http/Controllers/TortugaController.php
+++ app/Http/Controllers/TortugaController.php
@@ -5,62 +5,63 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTortugaRequest;
 use App\Http\Requests\UpdateTortugaRequest;
 use App\Models\Tortuga;
+use Illuminate\View\View;
 
 class TortugaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(): View
+  {
+    return view('tortugas.index');
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreTortugaRequest $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreTortugaRequest $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Tortuga $tortuga)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Tortuga $tortuga)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Tortuga $tortuga)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Tortuga $tortuga)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateTortugaRequest $request, Tortuga $tortuga)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateTortugaRequest $request, Tortuga $tortuga)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Tortuga $tortuga)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Tortuga $tortuga)
+  {
+    //
+  }
 }
---
+++ resources/views/layouts/tortugas/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.base')
+
+@section('title', 'Our Turtles (and tortois)')
+
+
+@section('content')
+<h1>Our Turtles</h1>
+@endsection
