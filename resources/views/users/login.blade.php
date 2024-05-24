ba16ad94a70965c5ad602c1ebb070bbc57ca97d5
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-24 09:22:11 +0200

    Display errors CSRF en los formularios de registro y login

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,27 +1,34 @@
- @extends('layouts.base')
+@extends('layouts.base')
 
- @section('title', 'Login')
+@section('title', 'Login')
 
+@section('content')
+<div class="md:flex md:justify-center md:gap-10 md:items-center">
+  <div class="md:w-4/12 bg-white shadow-xl rounded-lg p-6">
+    <img src="{{ asset('img/tortuga-george.jpg') }}" alt="Login Turtle" />
+  </div>
+  <div class="md:w-4/12 bg-white shadow-xl rounded-lg p-6">
+    <form method="POST" action="{{ route('login') }}">
+      @csrf
 
- @section('content')
- <div class="md:flex md:justify-center md:gap-10 md:items-center">
-   <div class="md:w-4/12 bg-white shadow-xl rounded-lg p-6">
-     <img src="{{ asset('img/tortuga-george.jpg') }}" alt="Login Turtle" />
-   </div>
-   <div class="md:w-4/12 bg-white shadow-xl rounded-lg p-6">
-     <form>
-       <div>
-         <div>
-           <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Email</label>
-           <input type="email" name="email" placeholder="Your email" class="border p-3 w-full rounded-lg" />
-         </div>
-         <div>
-           <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Password</label>
-           <input type="password" name="password" placeholder="Your password" class="border p-3 w-full rounded-lg" />
-         </div>
+      <div class="mb-4">
+        <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Email</label>
+        <input type="email" name="email" id="email" placeholder="Your email" value="{{ old('email') }}" class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror" />
+        @error('email')
+        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+        @enderror
+      </div>
 
-         <input type="submit" value="Login" class="bg-green-600 hover:bg-green-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg mt-5" />
-     </form>
-   </div>
- </div>
- @endsection
+      <div class="mb-4">
+        <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Password</label>
+        <input type="password" name="password" id="password" placeholder="Your password" class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror" />
+        @error('password')
+        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+        @enderror
+      </div>
+
+      <input type="submit" value="Login" class="bg-green-600 hover:bg-green-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg mt-5" />
+    </form>
+  </div>
+</div>
+@endsection
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,35 +1,50 @@
- @extends('layouts.base')
+@extends('layouts.base')
 
- @section('title', 'Register')
+@section('title', 'Register')
 
+@section('content')
+<div class="md:flex">
+  <div class="md:w-1/2 bg-white shadow-xl rounded-lg p-6">
+    <img src="{{ asset('img/tortuga-register.jpg') }}" alt="Welcome turtle" />
+  </div>
+  <div class="md:w-1/2 bg-white shadow-xl rounded-lg p-6">
+    <form method="POST" action="">
+      @csrf
 
- @section('content')
- <div class="md:flex">
-   <div class="md:w-1/2 bg-white shadow-xl rounded-lg p-6">
-     <img src="{{ asset('img/tortuga-register.jpg') }}" alt="Welcome turtle" />
-   </div>
-   <div class="md:w-1/2 bg-white shadow-xl rounded-lg p-6">
-     <form>
-       <div>
-         <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Name</label>
-         <input type="text" name="name" placeholder="Your name" class="border p-3 w-full rounded-lg" />
-       </div>
+      <div class="mb-4">
+        <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Name</label>
+        <input type="text" name="name" id="name" placeholder="Your name" value="{{ old('name') }}" class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror" />
+        @error('name')
+        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+        @enderror
+      </div>
 
-       <div>
-         <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Email</label>
-         <input type="email" name="email" placeholder="Your email" class="border p-3 w-full rounded-lg" />
-       </div>
-       <div>
-         <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Password</label>
-         <input type="password" name="password" placeholder="Your password" class="border p-3 w-full rounded-lg" />
-       </div>
-       <div>
-         <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">Confirm Password</label>
-         <input type="password" name="password_confirmation" placeholder="Confirm Your password" class="border p-3 w-full rounded-lg" />
-       </div>
+      <div class="mb-4">
+        <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Email</label>
+        <input type="email" name="email" id="email" placeholder="Your email" value="{{ old('email') }}" class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror" />
+        @error('email')
+        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+        @enderror
+      </div>
 
-       <input type="submit" value="Register" class="bg-green-600 hover:bg-green-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg mt-5" />
-     </form>
-   </div>
- </div>
- @endsection
+      <div class="mb-4">
+        <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Password</label>
+        <input type="password" name="password" id="password" placeholder="Your password" class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror" />
+        @error('password')
+        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+        @enderror
+      </div>
+
+      <div class="mb-4">
+        <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">Confirm Password</label>
+        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Your password" class="border p-3 w-full rounded-lg" />
+        @error('password_confirmation')
+        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+        @enderror
+      </div>
+
+      <input type="submit" value="Register" class="bg-green-600 hover:bg-green-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg mt-5" />
+    </form>
+  </div>
+</div>
+@endsection
