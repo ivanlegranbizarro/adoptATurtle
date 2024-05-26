6e3871f17e89540b0005499e3ef55b92c0fa3b94
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-26 10:15:01 +0200

    Login finnished

--- app/Http/Controllers/AuthorizationController.php
+++ app/Http/Controllers/AuthorizationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
@@ -9,9 +10,18 @@ use Illuminate\View\View;
 
 class AuthorizationController extends Controller
 {
-  public function login(): View
+  public function index(): View
   {
     return view('users.login');
+  }
+
+  public function login(LoginRequest $request): RedirectResponse
+  {
+    $data = $request->validated();
+
+    auth()->attempt($data);
+
+    return redirect()->route('tortugas.index');
   }
 
 
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('registe');
 
-Route::get('/login', [AuthorizationController::class, 'login'])->name('login');
+Route::get('/login', [AuthorizationController::class, 'index'])->name('login');
+Route::post('/login', [AuthorizationController::class, 'login'])->name('login');
 Route::get('/logout', [AuthorizationController::class, 'logout'])->name('logout');
 
 
