269b183e72958b443405385fcb2a16f5ac987c65
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-27 18:30:07 +0200

    Estableciendo las relaciones de User y Tortuga con la tabla pivote Adopcion

--- app/Models/Adopcion.php
+++ app/Models/Adopcion.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Tortuga;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Adopcion extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+
+  protected $fillable = [
+    'user_id',
+    'tortuga_id',
+  ];
+
+
+  public function tortuga()
+  {
+    return $this->belongsTo(Tortuga::class);
+  }
+
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
--- app/Models/Tortuga.php
+++ app/Models/Tortuga.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Adopcion;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tortuga extends Model
 {
@@ -16,4 +17,9 @@ class Tortuga extends Model
     'birthday',
     'image',
   ];
+
+  public function adopcion()
+  {
+    return $this->hasOne(Adopcion::class);
+  }
 }
