736ac85a63c75b34410134d761c0d038354ea22e
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-27 11:09:12 +0200

    mejorando póliza de permisos

--- app/Policies/TortugaPolicy.php
+++ app/Policies/TortugaPolicy.php
@@ -27,25 +27,34 @@ class TortugaPolicy
   /**
    * Determine whether the user can create models.
    */
-  public function create(User $user): bool
+  public function create(User $user): bool|Response
   {
-    return true;
+    if ($user->hasRole('admin')) {
+      return true;
+    }
+    return Response::deny('You are not alloweed to register information about turtles.');
   }
 
   /**
    * Determine whether the user can update the model.
    */
-  public function update(User $user, Tortuga $tortuga): bool
+  public function update(User $user, Tortuga $tortuga): bool|Response
   {
-    return true;
+    if ($user->hasRole('admin')) {
+      return true;
+    }
+    return Response::deny('You are not alloweed to modify information about turtles.');
   }
 
   /**
    * Determine whether the user can delete the model.
    */
-  public function delete(User $user, Tortuga $tortuga): bool
+  public function delete(User $user, Tortuga $tortuga): bool|Response
   {
-    return true;
+    if ($user->hasRole('admin')) {
+      return true;
+    }
+    return Response::deny('You are not alloweed to delete information about turtles.');
   }
 
   /**
