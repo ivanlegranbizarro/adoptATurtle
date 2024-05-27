f26a9d5d74ece4cb23bb0c98b138a70ae2bd2448
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-27 18:43:42 +0200

    Policie para las adopciones

--- app/Policies/AdopcionPolicy.php
+++ app/Policies/AdopcionPolicy.php
@@ -8,59 +8,74 @@ use Illuminate\Auth\Access\Response;
 
 class AdopcionPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
+  /**
+   * Determine whether the user can view any models.
+   */
+  public function viewAny(User $user): bool|Response
+  {
+    if ($user->hasRole('admin')) {
+      return true;
     }
+    return Response::deny('You are not alloweed to check information about adoptions.');
+  }
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Adopcion $adopcion): bool
-    {
-        //
+  /**
+   * Determine whether the user can view the model.
+   */
+  public function view(User $user, Adopcion $adopcion): bool|Response
+  {
+    if ($user->hasRole('admin')) {
+      return true;
     }
+    return Response::deny('You are not alloweed to check information about adoptions.');
+  }
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
+  /**
+   * Determine whether the user can create models.
+   */
+  public function create(User $user): bool|Response
+  {
+    if ($user->hasRole('admin')) {
+      return true;
     }
+    return Response::deny('You are not alloweed to register information about adoptions.');
+  }
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Adopcion $adopcion): bool
-    {
-        //
+  /**
+   * Determine whether the user can update the model.
+   */
+  public function update(User $user, Adopcion $adopcion): bool|Response
+  {
+    if ($user->hasRole('admin')) {
+      return true;
     }
+    return Response::deny('You are not alloweed to modify information about adoptions.');
+  }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Adopcion $adopcion): bool
-    {
-        //
+  /**
+   * Determine whether the user can delete the model.
+   */
+  public function delete(User $user, Adopcion $adopcion): bool|Response
+  {
+    if ($user->hasRole('admin')) {
+      return true;
     }
+    return Response::deny('You are not alloweed to delete information about adoptions.');
+  }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Adopcion $adopcion): bool
-    {
-        //
-    }
+  /**
+   * Determine whether the user can restore the model.
+   */
+  public function restore(User $user, Adopcion $adopcion): bool
+  {
+    return false;
+  }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Adopcion $adopcion): bool
-    {
-        //
-    }
+  /**
+   * Determine whether the user can permanently delete the model.
+   */
+  public function forceDelete(User $user, Adopcion $adopcion): bool
+  {
+    return false;
+  }
 }
