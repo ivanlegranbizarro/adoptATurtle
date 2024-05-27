295327455f13e0620e901b8ad4680761c5abc2aa
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-27 18:21:02 +0200

    creacion tabla de Adopciones y añadir flag de adopcion al modelo Tortuga

---
+++ database/migrations/2024_05_27_162011_add_is_adopted_to_tortugas_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::table('tortugas', function (Blueprint $table) {
+      $table->boolean('is_adopted')->default(false);
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::table('tortugas', function (Blueprint $table) {
+      $table->dropColumn('is_adopted');
+    });
+  }
+};
