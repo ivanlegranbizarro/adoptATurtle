7a9eaf86344081af32b6d618a269dba8a80c6868
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-28 10:21:56 +0200

    arreglar el formulario y añadir ruta home

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\TortugaController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [TortugaController::class, 'index'])->name('tortugas.index');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('registe');
