9714435c7fddd40b7fd38dc9c8177d36c37dd8f7
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-28 10:33:11 +0200

    fixing form typos

--- resources/views/layouts/tortugas/create.blade.php
+++ resources/views/layouts/tortugas/create.blade.php
@@ -11,7 +11,8 @@
 
         <div class="mb-4">
           <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Name</label>
-          <input type="text" name="name" id="name" placeholder="Turtle name" value="{{ old('name') }}" class="border p-3 w-full rounded-lg" required minlength="2" />
+          <input type="text" name="name" id="name" placeholder="Turtle name" value="{{ old('name') }}"
+            class="border p-3 w-full rounded-lg" required minlength="2" />
           @error('name')
           <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
           @enderror
@@ -19,7 +20,8 @@
 
         <div class="mb-4">
           <label for="birthday" class="mb-2 block uppercase text-gray-500 font-bold">Birthday</label>
-          <input type="date" name="birthday" id="birthday" placeholder="Turtle's birthday" value="{{ old('birthday') }}" class="border p-3 w-full rounded-lg" required />
+          <input type="date" name="birthday" id="birthday" placeholder="Turtle's birthday" value="{{ old('birthday') }}"
+            class="border p-3 w-full rounded-lg" required />
           @error('birthday')
           <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
           @enderror
@@ -28,12 +30,13 @@
         <div class="mb-4">
           <label for="image" class="mb-2 block uppercase text-gray-500 font-bold">Image</label>
           <input type="file" name="image" id="image" class="border p-3 w-full rounded-lg" required accept="image/*" />
-          )
+          @error('image')
           <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
           @enderror
         </div>
 
-        <input type="submit" value="Create Turtle" class="bg-green-600 hover:bg-green-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg mt-5" />
+        <input type="submit" value="Create Turtle"
+          class="bg-green-600 hover:bg-green-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg mt-5" />
       </form>
     </turbo-frame>
   </div>
