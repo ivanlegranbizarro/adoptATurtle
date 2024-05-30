ad52623205d70537f9288d4fb479fbd24a7e2584
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-30 10:19:09 +0200

    Index de adopciones

--- app/Http/Controllers/AdopcionController.php
+++ app/Http/Controllers/AdopcionController.php
@@ -5,62 +5,63 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreAdopcionRequest;
 use App\Http\Requests\UpdateAdopcionRequest;
 use App\Models\Adopcion;
+use Illuminate\View\View;
 
 class AdopcionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(): View
+  {
+    return view('adopciones.index');
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreAdopcionRequest $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreAdopcionRequest $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Adopcion $adopcion)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Adopcion $adopcion)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Adopcion $adopcion)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Adopcion $adopcion)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateAdopcionRequest $request, Adopcion $adopcion)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateAdopcionRequest $request, Adopcion $adopcion)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Adopcion $adopcion)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Adopcion $adopcion)
+  {
+    //
+  }
 }
