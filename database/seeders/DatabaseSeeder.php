d363e15079541a0456e3e049d2cd4969a0d20e1b
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-01 07:23:20 +0200

    Añadiendo roles al user's factory y creando un admin y un usuario sin permisos para probar la página

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,27 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    User::factory()->create([
+      'name' => 'admin',
+      'email' => 'admin@admin.com',
+      'password' => Hash::make('password'),
+      'role' => 'admin'
+    ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    User::factory()->create([
+      'name' => 'ruben',
+      'email' => 'ruben@ruben.com',
+      'password' => Hash::make('password'),
+      'role' => 'user'
+    ]);
+  }
 }
