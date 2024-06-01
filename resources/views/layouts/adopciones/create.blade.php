9d758db3353005f8063689e119492815e2d2b6a2
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-01 08:52:34 +0200

    Mejorando errores en el formulario de adopción

--- resources/views/layouts/adopciones/create.blade.php
+++ resources/views/layouts/adopciones/create.blade.php
@@ -10,7 +10,10 @@
     <input type="hidden" name="tortuga_id" value="{{ $tortuga->id }}">
     <div class="mb-4">
       <label for="motivation" class="block text-gray-700">Motivation:</label>
-      <textarea name="motivation" id="motivation" rows="4" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm" placeholder="Why do you want to adopt {{ $tortuga->name }}? Please, provide as much information as possible. We need to know {{ $tortuga->name }} will be in good hands. "></textarea>
+      <textarea name="motivation" id="motivation" rows="4" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm" placeholder="Why do you want to adopt {{ $tortuga->name }}? Please, provide as much information as possible. We need to know {{ $tortuga->name }} will be in good hands.">@if(old('motivation')){{ old('motivation') }}@endif</textarea>
+      @error('motivation')
+      <p class="text-red-500 text-xs mt-1">Please, we need more details.</p>
+      @enderror
     </div>
     <button type="submit" class="bg-blue-300 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded transition-colors duration-200">
       Confirm Adoption
